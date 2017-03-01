023b6e19a4bff9ae2fc85a326c67069524394a47
acidmag <acidmagazine@gmail.com>
2017-02-28 22:55:46 -0500

    AM-1: Created basic components to test functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { InputBarComponent } from './input-bar/input-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    InputBarComponent
   ],
   imports: [
     BrowserModule,
