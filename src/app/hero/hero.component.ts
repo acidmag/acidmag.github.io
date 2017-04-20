356520fede1444fdb9808d1302489d2e204e2541
acidmag <acidmagazine@gmail.com>
2017-04-20 18:32:31 -0400

    AM-8: Compressed landing image

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -9,7 +9,7 @@ export class HeroComponent implements OnInit {
   fullImagePath: string;
 
   constructor() {
-      this.fullImagePath = '/assets/images/acid2.png';
+      this.fullImagePath = '/assets/images/acid2.jpg';
   }
 
   ngOnInit() {
