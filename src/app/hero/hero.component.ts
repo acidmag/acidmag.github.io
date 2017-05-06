8bf32100534388ae45f43e1869e03bb5b526e128
acidmag <acidmagazine@gmail.com>
2017-05-06 17:45:06 -0400

    AM-UI: added logo to the landing page, changed number of article cards on the main page

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeroComponent implements OnInit {
   bannerImagePath: string;
   backgroundImagePath: string;
+  logoImagePath: string;
 
   constructor() {
       this.bannerImagePath = '/assets/images/Homepage_Acid collage.png';
       this.backgroundImagePath = '/assets/images/Homepage_Glitch effect.png';
+      this.logoImagePath = '/assets/images/Homepage_Logo.png';
   }
 
   ngOnInit() {
