9796593dd03e161ab097fe1731b2f182174207ae
siddhantshah-apx <siddhant_shah@comcast.com>
2017-06-05 19:36:56 -0400

    removed space

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -5,7 +5,6 @@ import { ArticleService } from '../article.service';
 
 
 @Component({
-
   selector: 'article-card',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.css']
